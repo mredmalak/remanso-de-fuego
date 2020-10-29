265e10609d03c67960f61301dde220382e894816
germanmorenoortiz <germanmorenoortiz.com>
2020-10-29 13:54:21 +0100

    create a logger

--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from './lib/logger.js';
 //import routes from './config/routes';
 
 const HOST = './localhost';
 const PORT = 4000;
 
+
+
 const server = express();
 server.use(bodyParser.json());
 //server.use(routes);
+server.use(logger);
 
 const noEndPointHandler = (request, response, next) => {
-  console.log('touching endpoint');
+  response.statusCode = 404;
+  response.send({
+    message: 'Error: endpoint not found.',
+  });
 };
+
+
 
 server.get('*', noEndPointHandler);
 
