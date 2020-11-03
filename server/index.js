1216e6baa9467030c9c1ed9f03500abdca2ee452
germanmorenoortiz <germanmorenoortiz.com>
2020-11-03 16:42:19 +0100

    ultimo

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import logger from './lib/logger.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // middlewares
 import loggerMiddleware from './middleware/logger-middleware.js';
@@ -12,11 +15,11 @@ import jsonResponse from './middleware/json-response.js';
 import animoFraseRouter from './routes/animo-frase.js';
 import galeriaRouter from './routes/galeria.js';
 
-const HOST = 'localhost';
-const PORT = 4000;
+const HOST =process.env.HOST || 'localhost';
+const PORT =process.env.PORT || 4000;
 
-export const databaseURI = 'mongodb://localhost/remanso-de-fuego';
-// const databaseURI = 'mongodb://localhost:2700/remanso-de-fuego';
+export const databaseURI =process.env.DATABASE_URL || 'mongodb://localhost/remanso-de-fuego';
+
 
 mongoose.connect(databaseURI, {
   useFindAndModify: false,
